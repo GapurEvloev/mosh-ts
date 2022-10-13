f9754478336dcaee0c75db280c68acbf3088afbe
gapur <kaloycant@gmail.com>
2022-10-14 01:04:59 +0200

    configuring

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Hello world!");
+let a = 20;
+a = 30;
+// a = "22"; // => error
+// a = +"22"; // => correct
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log("Hello world!");
+let a: number = 20;
+a = 30;
+// a = "22"; // => error
+// a = +"22"; // => correct
